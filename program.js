e5a137be38394fda49447120f115260dcb3749c9
AiyanaCookey2024 <167078825+AiyanaCookey2024@users.noreply.github.com>
2024-04-22 12:10:38 +0000

    .

--- program.js
+++ program.js
@@ -1,22 +1,24 @@
-let tList = []; //
+let tList = []; 
 
-function x1(tName, tDue) {
-  let t = { n: tName, d: tDue, c: false };
+function createTask(tName, tDue) {
+ let t = { n: tName, d: tDue, c: false }
+ console.log("createTask " + t.n)
   tList.push(t);
+  
 }
 
-function x2(tName) {
-  tList = tList.filter(t => t.n !== tName);
+function removeTask(tName) {
+  tList = tList.filter(t => t.n !== tName); //this is just removing the name 
 }
 
-function x3() {
+function printAllTasks() {
   console.log("All Tasks:");
   tList.forEach(t => {
     console.log(`Task: ${t.n}, Due: ${t.d}, Completed: ${t.c}`);
   });
 }
 
-function x4(tName) {
+function completeTask(tName) {
   let tIndex = tList.findIndex(t => t.n === tName);
   if (tIndex !== -1) {
     tList[tIndex].c = true;
@@ -26,9 +28,9 @@ function x4(tName) {
 }
 
 
-x1("Fix bug in code", "2024-02-21");
-x1("Update documentation", "2024-02-22");
-x3();
-x4("Fix bug in code");
-x2("Update documentation");
-x3();
+createTask("Fix bug in code", "2024-02-21");
+createTask("Update documentation", "2024-02-22");
+printAllTasks();
+completeTask("Fix bug in code");
+removeTask("Update documentation");
+printAllTasks();
